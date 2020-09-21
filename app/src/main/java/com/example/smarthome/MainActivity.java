c6be31330fafc5c83b21196635e1837873f4e722
Daniil <danik22122005@gmail.com>
2020-09-21 18:00:23 +0300

    bug

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.smarthome.Network.ImageRequester;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ImageRequester imageRequester;
+    private NetworkImageView editImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        imageRequester = ImageRequester.getInstance();
+        editImage = findViewById(R.id.chooseImage);
+
         setContentView(R.layout.activity_main);
+        imageRequester.setImageFromUrl(editImage, "https://img.kub.in.ua/webp_cache/f1e3a7245f/577b4748a670671bc9dbb74656c84f05-700-580-default.webp");
+
     }
 }
