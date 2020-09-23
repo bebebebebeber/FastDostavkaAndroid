24e5ec2d616b03c2044e14314e10f759dc42f5db
Daniil <danik22122005@gmail.com>
2020-09-23 13:28:59 +0300

    sigin with google

--- app/src/main/java/com/example/smarthome/MainActivity.java
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -20,6 +20,14 @@ import com.example.smarthome.Network.Login;
 import com.example.smarthome.Network.NetworkService;
 import com.example.smarthome.Network.Tokens;
 import com.example.smarthome.constants.Urls;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -32,21 +40,23 @@ public class MainActivity extends AppCompatActivity {
     //private ImageRequester imageRequester;
     private NetworkImageView editImage;
     private Button loginButton;
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient mGoogleSignInClient;
+
     public void saveJWTToken(String token) {
         SharedPreferences prefs;
         SharedPreferences.Editor edit;
-        prefs=this.getSharedPreferences("jwtStore", Context.MODE_PRIVATE);
-        edit=prefs.edit();
+        prefs = this.getSharedPreferences("jwtStore", Context.MODE_PRIVATE);
+        edit = prefs.edit();
         try {
-            edit.putString("token",token);
-            Log.i("Login",token);
+            edit.putString("token", token);
+            Log.i("Login", token);
             edit.commit();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +66,23 @@ public class MainActivity extends AppCompatActivity {
         //editImage =findViewById(R.id.photo);
         //NetworkImageView nv = (NetworkImageView) findViewById(R.id.photo);
         //imageRequester.setImageFromUrl(editImage, Urls.BASE_URL+"Images/div4una.jpg");
-
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+//        SignInButton signInButton = findViewById(R.id.google_button);
+//        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        SignInButton button = findViewById(R.id.google_button);
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                                          startActivityForResult(signInIntent, 1488);
+                                      }
+                                  }
+        );
     }
 
     public void click(View view) {
@@ -64,10 +90,9 @@ public class MainActivity extends AppCompatActivity {
         final TextInputEditText email = findViewById(R.id.email);
         final TextInputLayout passwordLayout = findViewById(R.id.passwordLayout);
         final TextInputLayout emailLayout = findViewById(R.id.emailLayout);
-        if(email.getText().toString()==""||password.getText().toString()==""){
+        if (email.getText().toString() == "" || password.getText().toString() == "") {
             passwordLayout.setError("Fill all fields!");
-        }
-        else{
+        } else {
             passwordLayout.setError("");
         }
 
@@ -86,15 +111,14 @@ public class MainActivity extends AppCompatActivity {
                         //textView.append(post.getUserId() + "\n");
                         //textView.append(post.getTitle() + "\n");
                         //textView.append(post.getBody() + "\n");
-                        if(response.errorBody()==null&&response.isSuccessful()){
+                        if (response.errorBody() == null && response.isSuccessful()) {
                             passwordLayout.setError("");
                             //loginButton.setError("");
                             Tokens post = response.body();
                             Toast toast = Toast.makeText(getApplicationContext(), "All done! your ref token :" + post.getRefreshToken(), Toast.LENGTH_LONG);
                             toast.show();
                             saveJWTToken(post.getToken());
-                        }
-                        else{
+                        } else {
                             //emailLayout.setError("");
                             //password.setError("Login or password was wrong");
                             passwordLayout.setError("Login or password was wrong");
@@ -110,7 +134,38 @@ public class MainActivity extends AppCompatActivity {
                         t.printStackTrace();
                     }
                 });
+
 //        Intent intent = new Intent(this, MenuSideBarActivity.class);
 //        startActivity(intent);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1488) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast toast = Toast.makeText(getApplicationContext(), "All done! your email:" + account.getEmail(), Toast.LENGTH_LONG);
+            toast.show();
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
     }
 }
